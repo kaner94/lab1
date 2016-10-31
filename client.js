137c76be1d4304f4c4a6d939634f024477f37281
kaner94 <kanery@tcd.ie>
2016-10-31 11:19:18 +0000

    Add connect and data listener.

--- client.js
+++ client.js
@@ -3,4 +3,15 @@ var net = require("net");
 var HOST = '10.62.0.121';
 var PORT = 9000;
 
+var socket = new net.Socket();
+
+socket.connect(PORT, HOST, function(){
+	console.log("Connected!\n");
+
+	socket.write("/echo.php?message=hello");
+});
+
+socket.on('data', function(data){
+	console.log("RECEIVED:  %s\n\n", data);
+});
 
