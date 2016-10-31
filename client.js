eb671893b4acf8e181fd16eb74ea80f75aa631ce
kaner94 <kanery@tcd.ie>
2016-10-31 11:27:32 +0000

    Fix GET request. Needed HTTP/1.1

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
+/*
+Distributed Systems - Lab1
+Ryan Kane - 1332 5208
+NodeJS
+*/
+
 var net = require("net");
 
 var HOST = '10.62.0.121';
-var PORT = 9000;
+var PORT = 8000;
 
 var socket = new net.Socket();
 
 socket.connect(PORT, HOST, function(){
 	console.log("Connected!\n");
 
-	socket.write("/echo.php?message=hello");
+	socket.write("GET /echo.php?message=this_should_work HTTP/1.1\r\n\r\n");
 });
 
 socket.on('data', function(data){
